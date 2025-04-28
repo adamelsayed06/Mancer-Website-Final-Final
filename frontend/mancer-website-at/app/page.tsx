82c54fa3ecaab4933f20957670cd163d5549f888
adamelsayed06 <adamelsayyed06@gmail.com>
2025-04-27 20:41:23 -0400

    fixed fetch

--- frontend/mancer-website-at/app/page.tsx
+++ frontend/mancer-website-at/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     const job   = (form.elements.namedItem('job')   as HTMLInputElement).value
 
     try {
-      const res = await fetch(`${process.env.FLASK_API}`, {
+      const res = await fetch(`${process.env.FLASK_API}/api/waitlist`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name, email, job }),
